e7b71b1fecfc978cd61c1e02f389d32c9ced77a9
源?깅? <ksm@imrbiz.co.kr>
2022-06-10 12:37:09 +0900

    Updated: ng-optimize-angular

--- liss.js
+++ liss.js
@@ -1,65 +1,70 @@
 #!/usr/bin/env node
 
 const cwdFileExists = require('./utils/cwd-file-exists.js');
-const getParameter  = require('./utils/get-parameter');
-const packageJson   = require('./package.json');
-const fetch         = require('./fetch.js');
+const getParameter = require('./utils/get-parameter');
+const packageJson = require('./package.json');
+const fetch = require('./fetch.js');
 
-console.log(`node-liss@${packageJson.version}`);
+const {version} = packageJson;
+console.log(`node-liss@${version}`);
 
 function run(platform) {
-	const detectFiles   = {
-		ng  : 'angular.json',
-		ts  : 'tsconfig.json',
-		node: 'package.json'
-	}
-	const detectedFiles = {};
+    const detectFiles = {
+        ng: 'angular.json',
+        ts: 'tsconfig.json',
+        node: 'package.json'
+    }
+    const detectedFiles = {};
 
-	Object.keys(detectFiles).some(platform_ => {
-		const file = detectFiles[platform_];
-		if (cwdFileExists(file)) {
-			detectedFiles[platform_] = true;
-			if (!platform) {
-				platform = platform_;
-			}
-		}
-	});
+    Object.keys(detectFiles).some(platform_ => {
+        const file = detectFiles[platform_];
+        if (cwdFileExists(file)) {
+            detectedFiles[platform_] = true;
+            if (!platform) {
+                platform = platform_;
+            }
+        }
+    });
 
-	if (platform) {
-		if (detectedFiles[platform]) {
-			switch (platform) {
-				case 'ng':
-					return require('./ng/liss.js');
-				case 'ts':
-					return require('./ts/liss.js');
-				case 'node':
-					return require('./node/liss.js');
-			}
-		} else {
-			console.warn(`'${detectFiles[platform]}' file is required.`);
-			return;
-		}
-	}
+    if (platform) {
+        if (detectedFiles[platform]) {
+            switch (platform) {
+                case 'ng':
+                    return require('./ng/liss.js');
+                case 'ts':
+                    return require('./ts/liss.js');
+                case 'node':
+                    return require('./node/liss.js');
+            }
+        } else {
+            console.warn(`'${detectFiles[platform]}' file is required.`);
+            return;
+        }
+    }
 
-	console.warn('Cannot Found Supported Platform');
+    console.warn('Cannot Found Supported Platform');
 }
 
 fetch('https://registry.npmjs.org/-/package/node-liss/dist-tags')
-	.then(response => response.json())
-	.then((distTags) => {
-		const {latest} = distTags;
-		if (latest !== packageJson.version) {
-			console.info(`\tFound New Version`)
-			console.info(`\tnpm i node-liss@${latest}`);
-		}
-	})
-	.catch(reason => {
-		// console.log(reason);
-	})
-	.finally(() => {
-		run(
-			getParameter({
-				matches: ['node', 'ts', 'ng'],
-			})
-		)
-	});
+    .then(response => response.json())
+    .then((distTags) => {
+        const {latest} = distTags;
+        if (latest !== version) {
+            if (parseFloat(latest) < parseFloat(version)) {
+                console.warn('\tNext Version');
+            } else {
+                console.info(`\tFound New Version`)
+                console.info(`\tnpm i node-liss@${latest}`);
+            }
+        }
+    })
+    .catch(reason => {
+        // console.log(reason);
+    })
+    .finally(() => {
+        run(
+            getParameter({
+                matches: ['node', 'ts', 'ng'],
+            })
+        )
+    });
---
+++ ng/ng-optimize-angular.js
@@ -0,0 +1,59 @@
+const getConfigure = require('../core/get-configure.js');
+const ngProjects = require("./utils/ng-projects");
+const cwdRequire = require("../utils/cwd-require");
+const cwdWriteJson = require("../utils/cwd-write-json");
+
+const angularJson = cwdRequire('angular.json');
+
+const {ng: {optimizeAngular: {styles: optimizeStyles, assets: optimizeAssets}}} = getConfigure({
+    ng: {
+        optimizeAngular: {
+            styles: [],
+            assets: []
+        }
+    }
+});
+
+Object.entries(ngProjects('application')).forEach(
+    ([key, project]) => {
+        const build = project['architect']['build'];
+        /**
+         *
+         * @type {string[]}
+         */
+        const styles = build['styles'] || [];
+        const assets = build['assets'] || [];
+
+        optimizeStyles.forEach(optimizeStyle => {
+            if (!styles.includes(optimizeStyle)) {
+                console.info(`[${key}] Add Style ${optimizeStyle}`);
+                styles[styles.length] = optimizeStyle;
+            }
+        })
+
+        optimizeAssets.forEach(optimizeAsset => {
+            if (typeof optimizeAsset === 'string') {
+                if (!assets.includes(optimizeAsset)) {
+                    console.info(`[${key}] Add Asset ${optimizeAsset}`);
+                    assets[assets.length] = optimizeAsset;
+                }
+            } else {
+                const {input, output} = optimizeAsset;
+                const hasOuterAsset = assets.some(asset => {
+                    if (typeof asset !== 'string' && asset['input'] === input) {
+                        return true;
+                    }
+                });
+
+                if (!hasOuterAsset) {
+                    console.info(`[${key}] Add Asset ${input} to ${output}`);
+                    assets[assets.length] = optimizeAsset;
+                }
+            }
+        });
+
+        angularJson['projects'][key] = project;
+    }
+);
+
+cwdWriteJson('angular.json', angularJson);
